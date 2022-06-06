f4534b18a85c88f6b5967e2dfd1bf3b44c0f1838
Kenny Clare <kmitchellclare@gmail.com>
2022-06-06 16:05:38 -0700

    commit tree 51259e6306a6a4446c758c2cb18b032c348d51ab

--- methods.rb
+++ methods.rb
@@ -1,1 +1,62 @@
 # Your code here!
+
+
+#function myFunction(param) {
+   # console.log("Running myFunction");
+   # return param +
+#}
+
+
+
+# def my_method(param)
+#     puts "Running my_method"
+#     param + 1
+# end
+
+
+
+
+# def say_hi(name = "kenny")
+#     puts "Hi there, #{name}"
+# end
+
+
+# def add_and_return(num1, num2)
+#     return num1 + num2
+# end
+
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+
+
+def add(num1, num2) 
+    num1 + num2
+end
+
+
+def halve(num)
+    return nil unless num.class == Integer
+    num / 2
+    end
+
+    # def halve(num)
+    #     if(number.class != Integer) 
+    #         return nil 
+    #     else
+    #         return number / 2
+    # end
